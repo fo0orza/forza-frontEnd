44da57271f62c624e6a94760458f50b9034f498a
Mohamed Sayed <mhmdsayed90031@gmail.com>
2026-05-06 19:59:41 +0300

    Add shadcn ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import "styles/tailwind.css"
+import { Figtree } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const figtree = Figtree({subsets:['latin'],variable:'--font-sans'});
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", figtree.variable)}>
       <body>{children}</body>
     </html>
   )

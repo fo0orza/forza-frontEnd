78f4f7363753879393ba9ac4a4244669e75dc8c9
Mohamed Sayed <mhmdsayed90031@gmail.com>
2026-06-01 10:31:27 +0300

    fix cta icons direction

--- components/shared/hero/hero-content/cta.tsx
+++ components/shared/hero/hero-content/cta.tsx
@@ -1,19 +1,22 @@
 import { ArrowRight, Play } from "lucide-react"
-import { getTranslations } from "next-intl/server";
+import { getLocale, getTranslations } from "next-intl/server";
 import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils";
 
 const Cta = async () => {
     const t = await getTranslations("Hero.cta");
+    const locale = await getLocale();
+    const isRtl = locale === "ar";
     return (
         <div className="flex flex-col sm:flex-row gap-3 items-center mt-8">
             <Button variant={"secondary"} className="px-5 py-5 capitalize">
-                {t("primary")} <ArrowRight />
+                {t("primary")} <ArrowRight className={isRtl ? "rotate-180" : ""} />
             </Button>
-            <Button className="px-5 py-5 capitalize bg-background/70 backdrop-blur-sm text-primary hover:bg-background/50 border border-primary/10">
+            <Button className={cn("px-5 py-5 capitalize bg-background/70 backdrop-blur-sm text-primary hover:bg-background/50 border border-primary/10" , isRtl ? "flex-row-reverse" : "")}>
                 {t("secondary")} <Play />
             </Button>
         </div>
     )
-}
+} 
 
 export default Cta

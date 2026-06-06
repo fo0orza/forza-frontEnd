0195b50f2005e6d7bfc5985a6741c11f1015776e
Mohamed Sayed <mhmdsayed90031@gmail.com>
2026-06-06 03:47:11 +0300

    chore : remove animated particales

--- widgets/hero/hero-background/index.tsx
+++ widgets/hero/hero-background/index.tsx
@@ -2,7 +2,6 @@ import GridPattern from "@/components/shared/grid-pattern"
 import BackLight from "@/widgets/hero/hero-background/back-light"
 import Fade from "@/widgets/hero/hero-background/fade"
 import GlowCircle from "@/widgets/hero/hero-background/glow-circle/glow-circle"
-import Particles from "@/widgets/hero/hero-background/particles"
 
 
 
@@ -10,7 +9,6 @@ const HeroBackground = () => {
     return (
         <div aria-hidden="true" className="absolute inset-0 z-1 overflow-hidden">
             <GridPattern />
-            <Particles />
             <BackLight />
             <GlowCircle />
             <Fade />

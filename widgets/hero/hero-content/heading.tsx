66632a099bfc863245894b1c8ef196168fcff182
Mohamed Sayed <mhmdsayed90031@gmail.com>
2026-06-06 03:49:51 +0300

    decrease text-shadow of hero heading

--- widgets/hero/hero-content/heading.tsx
+++ widgets/hero/hero-content/heading.tsx
@@ -6,7 +6,7 @@ const Heading = async () => {
         <h1 id="hero-heading" className="text-center mt-4 font-bold uppercase text-[clamp(3.5rem,9vw,7rem)] leading-[1.15] tracking-tight rtl:font-cairo ltr:font-oswald">
             <span className="text-primary">{t("line1")}</span>
             <br />
-            <span style={{ textShadow: "0 0 40px #009e52" }} className="text-secondary">{t("line2")}</span>
+            <span style={{ textShadow: "0 0 20px #009e52" }} className="text-secondary">{t("line2")}</span>
         </h1>
     )
 }
